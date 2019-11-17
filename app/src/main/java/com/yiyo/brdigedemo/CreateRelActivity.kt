d565632712cca71c6ffe0a2ea80bfcb059697fe8
Edgar Vazquez <edgar21_9@hotmail.com>
2019-11-16 22:38:25 -0600

    added relation function@

--- app/src/main/java/com/yiyo/brdigedemo/CreateRelActivity.kt
+++ app/src/main/java/com/yiyo/brdigedemo/CreateRelActivity.kt
@@ -2,11 +2,35 @@ package com.yiyo.brdigedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import bridgedb.NetworkEdge
+import bridgedb.NetworkEdgeOrBuilder
+import bridgedb.NetworkNode
+import com.yiyo.brdigedemo.bridgedb.BridgeDbBlocking
 
 class CreateRelActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_rel)
     }
+
+    fun onSubmit(v: View) {
+        val destEditText = findViewById<EditText>(R.id.destEditText)
+        val originEditText = findViewById<EditText>(R.id.originEditText)
+        if (destEditText.text.isBlank() || destEditText.text.isBlank()) {
+            return
+        }
+        val destinationId = destEditText.text.toString()
+        val originId = originEditText.text.toString()
+        val db = MainActivity.dbBlocking
+        val edge = NetworkEdge.newBuilder()
+            .setDestination(NetworkNode.newBuilder().setId(destinationId.toLong()).build())
+            .setOrigin(NetworkNode.newBuilder().setId(originId.toLong()).build())
+            .build()
+        db.createEdge(edge)
+    }
+
 }
